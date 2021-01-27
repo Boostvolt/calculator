745c03e5235833f40b8f6cbbe4b3deedb29e4fce
Jan <51777660+Boostvolt@users.noreply.github.com>
2021-01-27 21:12:21 +0100

    Initial Commit

---
+++ Rechner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Rechner
+//
+//  Created by Jan Kott on 27.01.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Rechner/RechnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RechnerApp.swift
+//  Rechner
+//
+//  Created by Jan Kott on 27.01.21.
+//
+
+import SwiftUI
+
+@main
+struct RechnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
